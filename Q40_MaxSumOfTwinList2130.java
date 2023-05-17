30623b21d2156cabffb29c98869c159775902478
tanisha <tanishamodi12@gmail.com>
2023-05-17 18:29:04 +0530

    list

---
+++ Q40_MaxSumOfTwinList2130.java
@@ -0,0 +1,30 @@
+// link to the question --> https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
+
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int maxVal = 0;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode nextNode, prev = null;
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        while (prev != null) {
+            maxVal = Math.max(maxVal, head.val + prev.val);
+            prev = prev.next;
+            head = head.next;
+        }
+
+        return maxVal;
+    }
+}
