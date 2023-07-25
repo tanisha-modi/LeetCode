63fd7b0c72a507136692cc2f9a0b95d78a9aac40
tanisha <tanishamodi12@gmail.com>
2023-07-25 15:14:53 +0530

    daily ques

---
+++ Q109_PeakIndexinMountainArray852.java
@@ -0,0 +1,19 @@
+//  link to the question --> https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid + 1] < arr[mid]){
+                end = mid;
+            }else{
+                start = mid +1;
+            }
+        }
+        return start;
+    }
+}
