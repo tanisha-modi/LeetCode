3bef84bb688ad580dca89da7a180da590d6fc3c6
tanisha <tanishamodi12@gmail.com>
2023-08-11 16:19:51 +0530

    daily ques

---
+++ Q128_CoinChangeII518.java
@@ -0,0 +1,16 @@
+// link to the question --> https://leetcode.com/problems/coin-change-ii/description/
+
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
+    }
+}
